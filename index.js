fbd04965da75b4a9266adef12ce52d7a53877e64
spin~ <65109978+spinfal@users.noreply.github.com>
2021-10-02 02:17:39 +0000

    Add .gitignore

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const session = require('express-session');
 const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
